ee4f6d30fad5d716221c4ff57eae72ef807c4a10
yagookkj <yagocamilo432@gmail.com>
2025-06-28 14:37:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da frança é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programção',
+    'O que é Python?',
+    'O python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Lingua inglsa',
+    'Como se diz olá em inglês?',
+    'Olá em ingles significa HELLO'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país do mundo é a Russía'
+)
